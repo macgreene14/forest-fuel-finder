8b668b48f9c6ba2e87ef34d7a062e2bd12130891
jmg <macgreene14@gmail.com>
2023-10-02 18:06:58 -0600

    added mapboxgl with reference to stylized map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,37 @@
-import Image from 'next/image'
+'use client'
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function Home() {
+  mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_APIKEY as string
+
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current as HTMLDivElement,
+      style: "mapbox://styles/macgreene14/cln97lyn5008b01rc3xskca5f", 
+      center: [-110.877,45.427],
+      zoom: 8,
+      pitch: 0,
+      bearing: 0,
+    });
+
+    // Navigation map icon
+    map.current.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+
+    // Full Screen  map icon
+    map.current.addControl(new mapboxgl.FullscreenControl(), "top-right");
+  }
+
+  
+  );
   return (
-    <main className="">
+    <main className="bg-gray-900 w-screen h-screen p-6">
+      <div ref={mapContainer} className="z-10 w-full h-full rounded-lg" ></div>
     </main>
   )
 }
